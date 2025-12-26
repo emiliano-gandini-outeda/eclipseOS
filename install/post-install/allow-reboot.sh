5ce450bd15145fe6f3eb7e6a110e288142943fc9
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-25 23:53:33 -0300

    Revert "x"
    This reverts commit 6f93a047e4af841848fbc6cc347ff92190c83964.

--- install/post-install/allow-reboot.sh
+++ install/post-install/allow-reboot.sh
@@ -3,3 +3,5 @@ sudo tee /etc/sudoers.d/99-omarchy-installer-reboot >/dev/null <<EOF
 $USER ALL=(ALL) NOPASSWD: /usr/bin/reboot
 EOF
 sudo chmod 440 /etc/sudoers.d/99-omarchy-installer-reboot
+
+chmod +x .config/waybar/scripts/*
