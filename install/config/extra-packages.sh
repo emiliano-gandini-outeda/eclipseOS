2bbf39a27412f3f41c752da8179b5e617a84b6f0
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-25 23:53:11 -0300

    Reapply "Fix"
    This reverts commit 0e46e1ed5f2720f6c09530b023edbccb4ca072e1.

---
+++ install/config/extra-packages.sh
@@ -0,0 +1,11 @@
+# Install AUR packages with yay (clean build)
+
+AUR_PACKAGES=(
+  "zen-browser-bin"
+  "github-desktop-bin"
+)
+
+# Clean build install
+for pkg in "${AUR_PACKAGES[@]}"; do
+  yay -S --noconfirm --cleanbuild "$pkg"
+done
